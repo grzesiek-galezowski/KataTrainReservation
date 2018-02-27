59c3e78f91feee08be6ba7ea155f0dcb398cb3f8
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-02-27 16:20:34 +0100

    Let's have a ticket convertible to dto
    (btw, how to hide this toDto method?)

--- Java/src/test/java/TicketOfficeSpecification.java
+++ Java/src/test/java/TicketOfficeSpecification.java
@@ -3,8 +3,10 @@ import autofixture.publicinterface.Any;
 import lombok.val;
 import org.testng.annotations.Test;
 import request.dto.ReservationRequestDto;
+import response.dto.TicketDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 public class TicketOfficeSpecification {
     
@@ -13,6 +15,10 @@ public class TicketOfficeSpecification {
         //WHEN
         val ticketOffice = new TicketOffice();
         val reservation = Any.anonymous(ReservationRequestDto.class);
+        val resultDto = Any.anonymous(TicketDto.class);
+
+        given(ticket.toDto()).willReturn(resultDto);
+
         //WHEN
         val ticketDto = ticketOffice.makeReservation(reservation);
 
