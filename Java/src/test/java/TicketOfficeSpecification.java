23002b0151dc0130c15a5b21effa037440123832
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-02-27 16:24:09 +0100

    discovered a Ticket interface and one method

--- Java/src/test/java/TicketOfficeSpecification.java
+++ Java/src/test/java/TicketOfficeSpecification.java
@@ -1,5 +1,6 @@
 import api.TicketOffice;
 import autofixture.publicinterface.Any;
+import logic.Ticket;
 import lombok.val;
 import org.testng.annotations.Test;
 import request.dto.ReservationRequestDto;
@@ -7,6 +8,7 @@ import response.dto.TicketDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 
 public class TicketOfficeSpecification {
     
@@ -16,6 +18,7 @@ public class TicketOfficeSpecification {
         val ticketOffice = new TicketOffice();
         val reservation = Any.anonymous(ReservationRequestDto.class);
         val resultDto = Any.anonymous(TicketDto.class);
+        Ticket ticket = mock(Ticket.class);
 
         given(ticket.toDto()).willReturn(resultDto);
 
