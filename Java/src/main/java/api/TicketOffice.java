517dbacaf02f9122898deaa73de9ad4f53256b79
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-02-28 16:31:35 +0100

    Passed the first assertion, but the second one fails
    Check the error message, Luke!

--- Java/src/main/java/api/TicketOffice.java
+++ Java/src/main/java/api/TicketOffice.java
@@ -2,21 +2,28 @@ package api;
 
 import logic.CommandFactory;
 import logic.TicketFactory;
+import lombok.val;
 import request.dto.ReservationRequestDto;
 import response.dto.TicketDto;
 
 public class TicketOffice {
 
     private CommandFactory commandFactory;
+    private TicketFactory ticketFactory;
 
-    public TicketOffice(CommandFactory commandFactory, TicketFactory ticketFactory) {
+    public TicketOffice(
+        CommandFactory commandFactory,
+        TicketFactory ticketFactory) {
         //todo implement
 
         this.commandFactory = commandFactory;
+        this.ticketFactory = ticketFactory;
     }
 
     public TicketDto makeReservation(
         ReservationRequestDto request) {
+        val ticket = ticketFactory.createBlankTicket();
+        val command = commandFactory.createBookCommand(request, ticket);
         return new TicketDto(null, null, null);
     }
 
