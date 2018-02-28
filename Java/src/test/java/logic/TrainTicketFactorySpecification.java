867552ef44b8f0c51a7fbdc7316f739b64c5e561
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-02-28 16:41:23 +0100

    THe factory should create a specific command for me

--- Java/src/test/java/logic/TrainTicketFactorySpecification.java
+++ Java/src/test/java/logic/TrainTicketFactorySpecification.java
@@ -1,6 +1,18 @@
 package logic;
 
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class TrainTicketFactorySpecification {
-    
+    @Test
+    public void shouldCreateBookingCommand() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+        assertThat(1).isEqualTo(2);
+    }
 
 }
