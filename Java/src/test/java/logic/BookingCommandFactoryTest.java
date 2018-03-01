08a4b6753369993e170395b3a54df1ad4ad53dea
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-01 07:57:53 +0100

    wrote a failing test for a type and dependencies

--- Java/src/test/java/logic/BookingCommandFactoryTest.java
+++ Java/src/test/java/logic/BookingCommandFactoryTest.java
@@ -1,5 +1,27 @@
 package logic;
 
-public class BookingCommandFactoryTest {
+import lombok.val;
+import org.testng.annotations.Test;
+import request.dto.ReservationRequestDto;
 
+import static com.github.grzesiek_galezowski.test_environment.types.ObjectGraphContainsDependencyCondition.dependencyOn;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class BookingCommandFactoryTest {
+    @Test
+    public void shouldCreateBookTicketCommand() {
+        //GIVEN
+        val bookingCommandFactory = new BookingCommandFactory();
+        val reservation = mock(ReservationRequestDto.class);
+        val ticket = mock(Ticket.class);
+
+        //WHEN
+        Command result = bookingCommandFactory.createBookCommand(reservation, ticket);
+
+        //THEN
+        assertThat(result).isInstanceOf(BookTicketCommand.class);
+        assertThat(result).has(dependencyOn(reservation));
+        assertThat(result).has(dependencyOn(ticket));
+    }
 }
