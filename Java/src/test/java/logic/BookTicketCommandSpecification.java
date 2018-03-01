0f3e0bdb151f4a65c6eb110c0dcd012624cf5ad4
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-01 08:04:30 +0100

    I have yet to discover what behavior I will require from the command

--- Java/src/test/java/logic/BookTicketCommandSpecification.java
+++ Java/src/test/java/logic/BookTicketCommandSpecification.java
@@ -1,5 +1,17 @@
 package logic;
 
-public class BookTicketCommandSpecification {
+import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BookTicketCommandSpecification {
+    @Test
+    public void shouldXXXXXXXXXXXXX() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+        assertThat(1).isEqualTo(2);
+    }
 }
