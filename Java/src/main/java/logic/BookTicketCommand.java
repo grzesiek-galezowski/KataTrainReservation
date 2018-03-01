ff7322cbe40c6a241c3422f133ff183241c5bd51
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-01 08:00:57 +0100

    Made 2nd assertion pass by introducing field for dto
    The 3rd assertion still fails.

--- Java/src/main/java/logic/BookTicketCommand.java
+++ Java/src/main/java/logic/BookTicketCommand.java
@@ -1,6 +1,14 @@
 package logic;
 
+import request.dto.ReservationRequestDto;
+
 public class BookTicketCommand implements Command {
+    private ReservationRequestDto reservation;
+
+    public BookTicketCommand(ReservationRequestDto reservation) {
+        this.reservation = reservation;
+    }
+
     @Override
     public void execute() {
         //todo implement
