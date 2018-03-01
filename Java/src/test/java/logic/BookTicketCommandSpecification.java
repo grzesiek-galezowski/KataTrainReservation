3f4c03be0f277c6f163015d00821df3cf80b2c3e
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-01 16:29:00 +0100

    Introduced collaborators and stated expectation

--- Java/src/test/java/logic/BookTicketCommandSpecification.java
+++ Java/src/test/java/logic/BookTicketCommandSpecification.java
@@ -1,21 +1,27 @@
 package logic;
 
+import autofixture.publicinterface.Any;
 import lombok.val;
 import org.testng.annotations.Test;
+import request.dto.ReservationRequestDto;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
 
 public class BookTicketCommandSpecification {
 
     @Test
     public void shouldXXXXXXXXXXXXX() {
         //GIVEN
+        val reservation = Any.anonymous(ReservationRequestDto.class);
+        val ticket = Any.anonymous(Ticket.class);
+        val train = mock(Train.class);
         val bookTicketCommand
             = new BookTicketCommand(reservation, ticket, train);
         //WHEN
         bookTicketCommand.execute();
 
         //THEN
-        assertThat(1).isEqualTo(2);
+        then(train).should().reserve(reservation.seatCount, ticket);
     }
 }
