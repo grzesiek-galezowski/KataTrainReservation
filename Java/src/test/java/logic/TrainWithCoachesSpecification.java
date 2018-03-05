27bf90f698aa81c05297e2b71f38878ed0081db4
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-05 15:26:14 +0100

    braindumping a new test

--- Java/src/test/java/logic/TrainWithCoachesSpecification.java
+++ Java/src/test/java/logic/TrainWithCoachesSpecification.java
@@ -1,6 +1,21 @@
 package logic;
 
-public class TrainWithCoachesSpecification {
+import lombok.val;
+import org.testng.annotations.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TrainWithCoachesSpecification {
+    @Test
+    public void shouldXXXXX() { //todo rename
+        //GIVEN
+        val trainWithCoaches = new TrainWithCoaches();
+
+        //WHEN
+        trainWithCoaches.reserve(seatCount, ticket);
+
+        //THEN
+        assertThat(1).isEqualTo(2);
+    }
 
 }
