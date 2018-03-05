66446796663bb988afc49961ef96a530bfd14aed
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-05 15:23:23 +0100

    Renaming a test (should've done this earlier)
    Should have left a TODO.

--- Java/src/test/java/logic/BookTicketCommandSpecification.java
+++ Java/src/test/java/logic/BookTicketCommandSpecification.java
@@ -11,7 +11,7 @@ import static org.mockito.Mockito.mock;
 public class BookTicketCommandSpecification {
 
     @Test
-    public void shouldXXXXXXXXXXXXX() {
+    public void shouldReserveSeatsOnTrainWhenExecuted() {
         //GIVEN
         val reservation = Any.anonymous(ReservationRequestDto.class);
         val ticket = Any.anonymous(Ticket.class);
