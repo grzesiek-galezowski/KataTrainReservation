ba7694a0585c44d2369f2a62810c94432ace4cae
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-05 08:14:42 +0100

    Made dummy implementation of TrainWithCoaches

--- Java/src/main/java/logic/CouchDbTrainRepository.java
+++ Java/src/main/java/logic/CouchDbTrainRepository.java
@@ -3,7 +3,6 @@ package logic;
 public class CouchDbTrainRepository implements TrainRepository {
     @Override
     public Train getTrainBy(String trainId) {
-        //todo implement
-        return null;
+        return new TrainWithCoaches();
     }
 }
---
+++ Java/src/main/java/logic/TrainWithCoaches.java
@@ -0,0 +1,9 @@
+package logic;
+
+public class TrainWithCoaches implements Train {
+    @Override
+    public void reserve(int seatCount, Ticket ticketToFill) {
+        //todo implement
+
+    }
+}
