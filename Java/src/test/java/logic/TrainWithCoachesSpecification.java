21c1858e1f48bd96782500d0dc7387ca8930d9c2
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-05 15:29:51 +0100

    passed the compiler
    Now time for some deeper thinking on the expectation

--- Java/src/test/java/logic/TrainWithCoachesSpecification.java
+++ Java/src/test/java/logic/TrainWithCoachesSpecification.java
@@ -1,15 +1,19 @@
 package logic;
 
+import autofixture.publicinterface.Any;
 import lombok.val;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class TrainWithCoachesSpecification {
     @Test
     public void shouldXXXXX() { //todo rename
         //GIVEN
         val trainWithCoaches = new TrainWithCoaches();
+        val seatCount = Any.intValue();
+        val ticket = mock(Ticket.class);
 
         //WHEN
         trainWithCoaches.reserve(seatCount, ticket);
