6b77d198cc4f0dd316a83cf2e7d9f327d77726ef
Galezowski Grzegorz-FTW637 <FTW637@motorolasolutions.com>
2018-03-09 16:35:33 +0100

    Discovered an interface Seat

--- Java/src/test/java/logic/CoachWithSeatsSpecification.java
+++ Java/src/test/java/logic/CoachWithSeatsSpecification.java
@@ -11,7 +11,18 @@ public class CoachWithSeatsSpecification {
     @Test
     public void xxXXxxXX() { //todo rename
         //GIVEN
-        val coachWithSeats = new CoachWithSeats();
+        Seat seat1 = Any.anonymous(Seat.class);
+        val coachWithSeats = new CoachWithSeats(
+            seat1,
+            seat2,
+            seat3,
+            seat4,
+            seat5,
+            seat6,
+            seat7,
+            seat8,
+            seat9
+        );
         //WHEN
         int seatCount = Any.intValue();
         val reservationAllowed = coachWithSeats.allowsReservationOf(seatCount);
